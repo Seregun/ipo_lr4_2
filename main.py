9d90c9f86ef9d734712cbc064abc8faa0b60a08d
Seregun <seregunmgp@gmail.com>
2024-10-06 15:19:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-numbers = [1, 2, 3, 4, 5] # Список чисел
-sqrt = [number**2 for number in numbers] # Возведение чисел  в квадрат
+numbers = input("Введите список чисел через запятую: ").split(',') # Ввод списка чисел
+sqrt = [int(num)**2 for num in numbers] # Возведение чисел  в квадрат
 print(sqrt) # Вывод чисел в квадрате
